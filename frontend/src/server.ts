7f76f538f8c8d0c0a02a95365ab44b20f0f39ba4
Arthur Lau <p134679528az@gmail.com>
2026-03-13 03:08:10 -0400

    chore: add http header

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -31,7 +31,7 @@ const apiUrl = process.env['API_URL'];
 if (apiUrl) {
   app.use('/v1', async (req: express.Request, res: express.Response) => {
     try {
-      const target = `${apiUrl}/v1${req.url}`;
+      const target = `https://${apiUrl}/v1${req.url}`;
       const body = req.method !== 'GET' && req.method !== 'HEAD'
         ? JSON.stringify(req.body)
         : undefined;
